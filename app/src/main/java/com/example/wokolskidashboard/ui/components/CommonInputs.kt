fc993abe283a53b9863c95c6a072070306c76642
Kuba Gawronski <jakub.gawronski@cyfroweszkoly.pl>
2026-04-26 20:24:15 +0200

    Add first Composable with WokulskiTextField function

--- app/src/main/java/com/example/wokolskidashboard/ui/components/CommonInputs.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/CommonInputs.kt
@@ -1,2 +1,21 @@
 package com.example.wokolskidashboard.ui.components
 
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WokulskiTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    modifier: Modifier = Modifier
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        modifier = modifier
+    )
+}
+
