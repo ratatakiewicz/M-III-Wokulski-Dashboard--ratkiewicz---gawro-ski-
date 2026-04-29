d29d91173d1e6df1132de6d8af6980ef0a0e0683
Kuba Gawronski <jakub.gawronski@cyfroweszkoly.pl>
2026-04-30 00:03:33 +0200

    Implement State Hoisting

--- app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
@@ -1,2 +1,25 @@
 package com.example.wokolskidashboard.ui
 
+import IncomeForm
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.wokolskidashboard.model.Transaction
+import com.example.wokolskidashboard.ui.components.*
+
+@Composable
+fun MainScreen() {
+
+    val transactions = remember { mutableStateListOf<Transaction>() }
+
+    var expenseName by remember { mutableStateOf("") }
+    var expenseAmount by remember { mutableStateOf("") }
+    var expenseCategory by remember { mutableStateOf("") }
+    var isUnnecessary by remember { mutableStateOf(false) }
+
+    var incomeName by remember { mutableStateOf("") }
+    var incomeAmount by remember { mutableStateOf("") }
+}
